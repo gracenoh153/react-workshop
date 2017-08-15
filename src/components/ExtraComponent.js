b80bfeaeba6f11ac2b0846a5c95ec2f8957156d5
Grace Noh <email": null>
2017-08-15 14:36:58 -0700

    make name appear on same line as 'Hello'

---
+++ src/components/ExtraComponent.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class ExtraComponent extends Component {
+  render(){
+    return(
+      <div>
+        This is an extra component. :)
+      </div>
+    )
+  }
+}
+
+export default ExtraComponent;
