bef01b4a796cdf32e018a04e89cd6be801fe5526
gracenoh153 <gracenoh153@gmail.com>
2017-08-17 01:10:22 -0700

    refactor to pass array objects as props

--- src/components/QuestionsAndAnswers.js
+++ src/components/QuestionsAndAnswers.js
@@ -2,15 +2,9 @@ import React, {Component} from 'react';
 
 class QuestionsAndAnswers extends Component {
   render(){
-    const questionsAndAnswers = [ {question: 'Where am I located?', 
-                                   answer: 'Seattle, WA'},
-                                  {question: 'What do I work in?',
-                                   answer: 'Rails for Back End and React for Front End!'},
-                                  {question: 'What is my favorite game?',
-                                   answer: 'Splinter Cell'}
-                                  ]
+
     return(<div>
-      {questionsAndAnswers.map(function({question, answer}){
+      {this.props.items.map(function({question, answer}){
         return(
         <div>
           <div className="question">
